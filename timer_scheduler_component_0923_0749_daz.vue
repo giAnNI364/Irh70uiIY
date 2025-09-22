498cd64b025a6b553f722fccf8bdf5c21fa4982d
giAnNI364 <sl7780481@gmail.com>
2025-09-23 01:49:48 +0200

    Setup new 定时任务调度器 feature

---
+++ timer_scheduler_component_0923_0749_daz.vue
@@ -0,0 +1,43 @@
+// 代码生成时间: 2025-09-23 07:49:47
+import { ref, onMounted, onUnmounted } from 'vue';
+
+// 响应式数据
+const taskName = ref('');
+const taskStatus = ref('未启动');
+let timer;
+
+// 开始任务的函数
+const startTask = () => {
+  taskName.value = '定时任务';
+  taskStatus.value = '运行中';
+  // 定时任务逻辑
+  timer = setInterval(() => {
+    // 这里可以放置定时任务的代码
+    console.log('任务正在运行...');
+  }, 1000);
+};
+
+// 停止任务的函数
+const stopTask = () => {
+  taskStatus.value = '已停止';
+  clearInterval(timer);
+};
+
+// 生命周期钩子
+onMounted(() => {
+  // 组件挂载后执行的代码
+  console.log('组件已挂载');
+});
+
+onUnmounted(() => {
+  // 组件卸载前执行的代码
+  if (timer) {
+    clearInterval(timer);
+  }
+  console.log('组件已卸载');
+});
+</script>
+
+<style scoped>
+/* 样式内容 */
+</style>
